7894769192f08d9352c298baab79de04849d379a
Teimur Gasanov <teymurgg321@gmail.com>
2022-11-26 14:24:32 +0100

    Add tests

--- packages/design-system/src/molecules/select/select.test.tsx
+++ packages/design-system/src/molecules/select/select.test.tsx
@@ -35,4 +35,52 @@ describe('Select', () => {
     )
     expect(onChange).toHaveBeenCalledWith('1')
   })
+
+  it('filters options', async () => {
+    const onChange = jest.fn()
+    const options = [
+      { name: 'Option #1', value: '1' },
+      { name: 'Option #2', value: '2' },
+      { name: 'Option #3', value: '3' },
+    ]
+    render(<Select options={options} onChange={onChange} />, {
+      wrapper: TryrollTestProvider,
+    })
+
+    const selectInput = await screen.findByTestId('selectInput')
+    expect(selectInput).toBeDefined()
+
+    fireEvent(selectInput, 'onFocus')
+
+    fireEvent.changeText(selectInput, '#2')
+
+    const option2 = await screen.findByTestId('selectOption__2')
+    expect(option2).toBeDefined()
+    const option1 = screen.queryByTestId('selectOption__1')
+    const option3 = screen.queryByTestId('selectOption__3')
+    expect(option1).toBeNull()
+    expect(option3).toBeNull()
+  })
+
+  it('clears value', async () => {
+    const onChange = jest.fn()
+    const options = [
+      { name: 'Option #1', value: '1' },
+      { name: 'Option #2', value: '2' },
+      { name: 'Option #3', value: '3' },
+    ]
+    render(<Select options={options} onChange={onChange} defaultValue="1" />, {
+      wrapper: TryrollTestProvider,
+    })
+
+    const selectInput = await screen.findByTestId('selectInput')
+    expect(selectInput).toBeDefined()
+
+    fireEvent(selectInput, 'onFocus')
+
+    // Simulate backspace change
+    fireEvent.changeText(selectInput, 'Option #')
+
+    expect(selectInput.props.value).toBe('')
+  })
 })
