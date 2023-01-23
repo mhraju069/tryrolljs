9f8fbb4bf3eeeb3e6888a2428c9d3b40a758120f
Diego Gonzalez <diego.ags04@gmail.com>
2023-01-23 12:40:50 -0400

    feat: Remove action on invalid network banner

--- packages/design-system/src/molecules/invalidNetworkBanner/index.tsx
+++ packages/design-system/src/molecules/invalidNetworkBanner/index.tsx
@@ -1,27 +1,11 @@
-import { useCallback } from 'react'
 import { useChainID } from '../../hooks'
 import { SUPPORTED_CHAIN_IDS } from '../../web3'
 import { Banner } from '../banner'
 
-type Props = {
+export type InvalidNetworkBannerProps = {
   title?: string
   supportedChainIDs?: number[]
   chainID?: number
-  validChainID: number
-}
-
-const changeNetwork = async (chainID: number) => {
-  const { ethereum } = window
-  if (ethereum && ethereum.request) {
-    try {
-      await ethereum.request({
-        method: 'wallet_switchEthereumChain',
-        params: [{ chainId: `0x${chainID}` }],
-      })
-    } catch (err) {
-      console.log(err)
-    }
-  }
 }
 
 const isSupportedNetwork = (
@@ -35,13 +19,8 @@ const isSupportedNetwork = (
 export const InvalidNetworkBanner = ({
   title,
   supportedChainIDs = SUPPORTED_CHAIN_IDS,
-  validChainID,
-}: Props) => {
+}: InvalidNetworkBannerProps) => {
   const chainID = useChainID()
-
-  const handleNetworkChange = useCallback(() => {
-    changeNetwork(validChainID)
-  }, [validChainID])
 
   if (!chainID || isSupportedNetwork(supportedChainIDs, chainID)) {
     return null
@@ -51,10 +30,6 @@ export const InvalidNetworkBanner = ({
     <Banner
       title={title ?? 'You are connected to the wrong network.'}
       variant="warning"
-      action={{
-        title: 'Click here to change network',
-        onPress: handleNetworkChange,
-      }}
     />
   )
 }
