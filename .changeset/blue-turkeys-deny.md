9fd0533f5266ccb29f4c5b3ac6138190af3d7009
Teimur Gasanov <teimur@tryroll.com>
2023-02-01 12:51:32 +0600

    Replace web3-react-core in favor of @web3-react/core

---
+++ .changeset/blue-turkeys-deny.md
@@ -0,0 +1,5 @@
+---
+'@tryrolljs/design-system': patch
+---
+
+Replace web3-react-core in favor of @web3-react/core
--- packages/design-system/src/providers/web3/index.tsx
+++ packages/design-system/src/providers/web3/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { Web3Provider as EthersWeb3Provider } from '@ethersproject/providers'
-import { Web3ReactProvider } from 'web3-react-core'
+import { Web3ReactProvider } from '@web3-react/core'
 import { AbstractConnector } from '@web3-react/abstract-connector'
 import {
   Web3ConnectorProvider,
