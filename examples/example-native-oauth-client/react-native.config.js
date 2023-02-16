eb5b3d5daf154f03209c07181a460f96584e1022
Teimur Gasanov <teimur@tryroll.com>
2023-02-16 20:26:37 +0600

    Add fonts

---
+++ examples/example-native-oauth-client/react-native.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  // https://www.npmjs.com/package/react-native-asset
+  assets: ['./node_modules/@tryrolljs/design-system/src/assets/fonts'],
+}
