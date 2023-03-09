a5c8f46ef9c62c0174412fe814887fe586f5ea4a
diegonzs-roll <106546965+diegonzs-roll@users.noreply.github.com>
2023-03-09 15:08:02 -0400

    feat: Change input layout implementation - Support multiple elements as children

--- packages/design-system/src/molecules/inputLayout/index.tsx
+++ packages/design-system/src/molecules/inputLayout/index.tsx
@@ -1,12 +1,11 @@
 import { useBreakpointValue } from 'native-base'
-import { forwardRef } from 'react'
-import { Platform, StyleSheet, TextInput, View } from 'react-native'
-import { InputPropsV2, InputV2 } from '../../molecules'
+import { PropsWithChildren } from 'react'
+import { Platform, StyleSheet, View } from 'react-native'
 import { TypographyV2 } from '../../atoms/typographyV2'
 import { useThemeV2 } from '../../hooks'
 import { container, margin } from '../../styles'
 
-export interface InputLayoutProps extends InputPropsV2 {
+export interface InputLayoutProps {
   title: string
   description: string
 }
@@ -15,42 +14,42 @@ const CONTENT_MAX_WIDTH = 448
 const INPUT_MAX_WIDTH = 544
 
 const styles = StyleSheet.create({
-  inputContainer: {
+  inputsContainer: {
     maxWidth: INPUT_MAX_WIDTH,
     flex: Platform.OS === 'web' ? 1 : undefined,
   },
 })
 
-export const InputLayout = forwardRef<TextInput, InputLayoutProps>(
-  ({ title, description, ...props }, ref) => {
-    const theme = useThemeV2()
-    const containerResponsiveStyles = useBreakpointValue({
-      md: [container.row, container.justifySpaceBetween, container.alignStart],
-    })
-    const contentResponsiveStyles = useBreakpointValue({
-      base: [margin.mb16],
-      md: [margin.mr24],
-    })
-    return (
-      <View style={[containerResponsiveStyles]}>
-        <View
-          style={[contentResponsiveStyles, { maxWidth: CONTENT_MAX_WIDTH }]}
+export const InputLayout: React.FC<InputLayoutProps & PropsWithChildren> = ({
+  title,
+  description,
+  children,
+}) => {
+  const theme = useThemeV2()
+  const containerResponsiveStyles = useBreakpointValue({
+    md: [container.row, container.justifySpaceBetween, container.alignStart],
+  })
+  const contentResponsiveStyles = useBreakpointValue({
+    base: [margin.mb16],
+    md: [margin.mr24],
+  })
+  return (
+    <View style={[containerResponsiveStyles]}>
+      <View style={[contentResponsiveStyles, { maxWidth: CONTENT_MAX_WIDTH }]}>
+        <TypographyV2
+          variant="sub3"
+          style={[margin.mb8]}
+          color={theme.text.black[100]}
         >
-          <TypographyV2
-            variant="sub3"
-            style={[margin.mb8]}
-            color={theme.text.black[100]}
-          >
-            {title}
-          </TypographyV2>
-          <TypographyV2 variant="text3" color={theme.text.black[80]}>
-            {description}
-          </TypographyV2>
-        </View>
-        <View style={[styles.inputContainer, { maxWidth: INPUT_MAX_WIDTH }]}>
-          <InputV2 ref={ref} {...props} />
-        </View>
+          {title}
+        </TypographyV2>
+        <TypographyV2 variant="text3" color={theme.text.black[80]}>
+          {description}
+        </TypographyV2>
       </View>
-    )
-  },
-)
+      <View style={[styles.inputsContainer, { maxWidth: INPUT_MAX_WIDTH }]}>
+        {children}
+      </View>
+    </View>
+  )
+}
--- packages/design-system/src/molecules/inputLayout/inputLayout.stories.tsx
+++ packages/design-system/src/molecules/inputLayout/inputLayout.stories.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import { View } from 'react-native'
 import { titleBuilder, fromTemplate } from '../../../.storybook/utils'
-import { padding } from '../../styles'
+import { container, margin, padding } from '../../styles'
+import { InputV2 } from '../inputV2'
 import { InputLayoutProps, InputLayout } from '.'
 
 const storyConfig = {
@@ -10,10 +11,27 @@ const storyConfig = {
 }
 
 const Template = (props: InputLayoutProps) => {
-  const [value, setValue] = useState('')
+  const [maxSupply, setMaxSupply] = useState('')
+  const [referral, setReferral] = useState('')
   return (
     <View style={[padding.p20]}>
-      <InputLayout {...props} value={value} onChangeText={setValue} />
+      <InputLayout {...props}>
+        <View style={container.fullWidth}>
+          <InputV2
+            label="Max supply"
+            placeholder="this is the place holder"
+            value={maxSupply}
+            onChangeText={setMaxSupply}
+          />
+          <View style={[margin.mv8]} />
+          <InputV2
+            label="Referral (optional)"
+            placeholder="Enter wallet address"
+            value={referral}
+            onChangeText={setReferral}
+          />
+        </View>
+      </InputLayout>
     </View>
   )
 }
@@ -22,8 +40,6 @@ export const Default = fromTemplate(Template, {
   title: 'Total token supply',
   description:
     'This is the total amount of tokens to be created. You can’t change the total supply once the token is created. Recommended supply: 10 million.',
-  label: 'Max supply',
-  placeholder: 'this is the place holder',
 })
 
 export default storyConfig
