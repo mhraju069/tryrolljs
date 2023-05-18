8845ba1c13ed13759192266bd50d014441b6d434
Teimur Gasanov <teimur@tryroll.com>
2023-05-18 16:10:08 +0600

    Add table component

---
+++ packages/design-system/src/organisms/table/index.ts
@@ -0,0 +1,4 @@
+export { default as Table } from './table'
+export { createColumnHelper } from '@tanstack/react-table'
+export type { PaginationState } from '@tanstack/react-table'
+export * from './types'
