9e307a1cafcc97cb125777ae3b7eb3e79b2eaaca
Teimur Gasanov <teimur@tryroll.com>
2023-06-07 19:20:22 +0600

    Remove useless code

--- examples/example-node-api-client/utils.ts
+++ examples/example-node-api-client/utils.ts
@@ -1,6 +1,3 @@
-import Client from '@tryrolljs/api-client'
-import SDK, { ScopeType } from '@tryrolljs/auth-sdk'
-
 export const makeMockStorage = () => {
   let storage: Record<string, string> = {}
 
@@ -21,57 +18,5 @@ export const makeMockStorage = () => {
       const keys = Object.keys(storage)
       return keys[i] || undefined
     },
-  }
-}
-
-export const makeSDK = () => {
-  return new SDK.default(
-    {
-      issuerUrl: process.env.ISSUER_URL || '',
-      clientId: process.env.CLIENT_ID || '',
-      clientSecret: process.env.CLIENT_SECRET || '',
-      scopes: [
-        ScopeType.ReadTx,
-        ScopeType.Offline,
-        ScopeType.Masquerade,
-        ScopeType.PlatformUser,
-      ],
-      redirectUrl: '',
-      logoutRedirectUrl: '',
-    },
-    makeMockStorage(),
-  )
-}
-
-export const generateAutoLoginTokenSDK = () => {
-  return new SDK.default(
-    {
-      apiUrl: process.env.API_URL || '',
-      issuerUrl: process.env.ISSUER_URL || '',
-      clientId: process.env.CLIENT_ID || '',
-      clientSecret: process.env.CLIENT_SECRET || '',
-      redirectUrl: process.env.REDIRECT_URL || '',
-      logoutRedirectUrl: process.env.REDIRECT_URL || '',
-      scopes: [
-        ScopeType.ReadTx,
-        ScopeType.Offline,
-        ScopeType.Masquerade,
-        ScopeType.PlatformUser,
-      ],
-    },
-    makeMockStorage(),
-  )
-}
-
-export const generateApiClient = async (sdk: SDK.default) => {
-  try {
-    await sdk.generateToken()
-
-    const apiClient = new Client.default({ baseUrl: process.env.API_URL }, sdk)
-
-    return apiClient
-  } catch (e) {
-    console.error(e)
-    throw e
   }
 }
