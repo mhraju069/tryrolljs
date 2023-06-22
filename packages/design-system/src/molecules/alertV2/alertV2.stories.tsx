6370e40d34357c0ce8518ec65286131c77994ac4
nsstarr <anastasia@tryroll.com>
2023-06-22 14:13:48 +0100

    Add a template for alertV2.stories

--- packages/design-system/src/molecules/alertV2/alertV2.stories.tsx
+++ packages/design-system/src/molecules/alertV2/alertV2.stories.tsx
@@ -1,8 +1,24 @@
 import { action } from '@storybook/addon-actions'
-import { titleBuilder } from '../../../.storybook/utils'
+import { titleBuilder, fromTemplate } from '../../../.storybook/utils'
 import { Alert } from '.'
 
 const conf = {
     title: titleBuilder.molecules('Alert'),
     component: Alert,
   }
+
+  const Template = (props: any) => (
+    <Alert {...props} />
+  )
+
+  export const Default = fromTemplate(Template, {
+    title: 'Alert',
+    variant: 'info'
+  })
+
+  export const Danger = fromTemplate(Template, {
+    title: 'Alert',
+    variant: 'danger'
+  })
+
+  export default conf
--- packages/design-system/src/molecules/alertV2/index.tsx
+++ packages/design-system/src/molecules/alertV2/index.tsx
@@ -1,5 +1,5 @@
 import { StyleProp, StyleSheet, View, ViewStyle } from 'react-native'
-import { Body } from '../../atoms'
+import { Body, TypographyV2 } from '../../atoms'
 import { margin, padding, container, makeStyles } from '../../styles'
 import { useThemeV2 } from '../../hooks';
 import { useMemo } from 'react';
@@ -10,31 +10,28 @@ type AlertProps = {
 };
 
 const styles = makeStyles({
-    container: {
-      minWidth: 320,
-    }
   })
 
 
 export const Alert = ({ title, variant }: AlertProps) => {
 const theme = useThemeV2()
-const colors: { background: string; text: string } = useMemo(() => {
-    if (variant === 'danger') {
-        return {
-            background: theme.background,
-            text: theme.text
-        }
-    }
-    return {
-        background: theme.background,
-        text: theme.text
-    }
-}, [variant, theme])
+// const colors: { background: string; text: string } = useMemo(() => {
+//     if (variant === 'danger') {
+//         return {
+//             background: theme.background,
+//             text: theme.text
+//         }
+//     }
+//     return {
+//         background: theme.background,
+//         text: theme.text
+//     }
+// }, [variant, theme])
 return (
-    <View>
-        <Body color={colors.text}>
+    <View style={styles}>
+        <TypographyV2 variant='text4' color={theme.text.black[80]}>
             {title}
-        </Body>
+        </TypographyV2>
     </View>
 )
 }
