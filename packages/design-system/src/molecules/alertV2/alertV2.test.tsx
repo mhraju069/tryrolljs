925e80c3fedb5cba572351fa1d0d4a8121872750
nsstarr <anastasia@tryroll.com>
2023-06-23 11:16:21 +0100

    Test: add test IDs to Alert component and test to check if the Alert component renders

---
+++ packages/design-system/src/molecules/alertV2/alertV2.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react-native'
+import { TryrollTestProvider } from '../../providers'
+import AlertV2 from './index'
+
+
+describe('AlertV2', () => {
+  beforeEach(() => {
+    render(
+      <AlertV2
+        title={'Once configured you’ll be required to enter both your password and an authentication code from your mobile phone in order to sign in.'}
+        variant={'info'}
+        showButton={true}
+        buttonText={'Label Button'}
+      />,
+      {
+        wrapper: TryrollTestProvider,
+      },
+    )
+  })
+
+  it('renders correctly', () => {
+    const container = screen.getByTestId('alertContainer')
+    const icon = screen.getByTestId('iconInfoCircle')
+    const text = screen.getByText('Once configured you’ll be required to enter both your password and an authentication code from your mobile phone in order to sign in.')
+    const button = screen.getByText('Label Button')
+
+    expect(container).toBeDefined()
+    expect(icon).toBeDefined()
+    expect(text).toBeDefined()
+    expect(button).toBeDefined()
+  })
+})
