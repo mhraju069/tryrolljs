8b7f80a26d858730e1c7f5c577e2de0558d486da
Teimur Gasanov <teimur@tryroll.com>
2023-06-27 19:52:00 +0600

    Fix test

--- packages/api-client/src/client.test.ts
+++ packages/api-client/src/client.test.ts
@@ -77,6 +77,7 @@ describe('client', () => {
     const authSdk_ = {
       ...authSdk,
       isTokenExpired: jest.fn().mockResolvedValue(true),
+      refreshToken: jest.fn().mockResolvedValue({}),
     }
     const client = new Client(defaultConfig, authSdk_)
 
