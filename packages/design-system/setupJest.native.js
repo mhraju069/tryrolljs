283e4297bc12645162c5f4721d477bb872123910
Teimur Gasanov <teimur@tryroll.com>
2023-07-06 19:48:03 +0600

    Fix tests

---
+++ packages/design-system/setupJest.native.js
@@ -0,0 +1,5 @@
+import 'mock-match-media/jest-setup'
+
+global.navigator = {
+  userAgent: 'node.js',
+}
--- packages/design-system/setupJest.web.js
+++ packages/design-system/setupJest.web.js
@@ -1,3 +1,10 @@
+import { TextEncoder, TextDecoder } from 'util'
+
+import 'mock-match-media/jest-setup'
+
+global.TextEncoder = TextEncoder
+global.TextDecoder = TextDecoder
+
 jest.mock('react-native-safe-area-context', () => {
   const inset = { top: 0, right: 0, bottom: 0, left: 0 }
   return {
@@ -8,18 +15,4 @@ jest.mock('react-native-safe-area-context', () => {
     SafeAreaView: jest.fn().mockImplementation(({ children }) => children),
     useSafeAreaInsets: jest.fn().mockImplementation(() => inset),
   }
-})
-
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: jest.fn().mockImplementation((query) => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(), // deprecated
-    removeListener: jest.fn(), // deprecated
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
 })
