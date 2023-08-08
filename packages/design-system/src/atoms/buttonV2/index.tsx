ab7969ffea9648208db1cc8b4db152a6d1eca758
nsstarr <anastasia@tryroll.com>
2023-08-08 14:20:33 +0100

    fix: remove ButtonV2 ref

--- packages/design-system/src/atoms/buttonV2/index.tsx
+++ packages/design-system/src/atoms/buttonV2/index.tsx
@@ -396,7 +396,6 @@ export const ButtonV2 = ({
   variant = 'primary',
   size = 'medium',
   textColor,
-  ref,
   ...props
 }: ButtonV2Props) => {
   const responsiveSize = useBreakpointValue({
@@ -412,7 +411,6 @@ export const ButtonV2 = ({
       variant={variant}
       size={responsiveSize}
       textColor={textColor}
-      ref={ref}
       {...baseButtonProps}
       {...props}
     />
--- packages/design-system/src/atoms/buttonV2/types.ts
+++ packages/design-system/src/atoms/buttonV2/types.ts
@@ -34,7 +34,6 @@ export interface ButtonV2Props {
   iconBackgroundColor?: string
   style?: StyleProp<ViewStyle>
   textColor?: string
-  ref: React.RefObject<any>
 }
 
 type States = 'rest' | 'hover' | 'active' | 'disabled'
