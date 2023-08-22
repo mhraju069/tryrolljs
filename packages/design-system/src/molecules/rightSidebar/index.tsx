030ce5018224cfbd13b3adba94160ced8d834074
Teimur Gasanov <teimur@tryroll.com>
2023-08-22 13:17:50 +0600

    Update view import

--- packages/design-system/src/molecules/rightSidebar/index.tsx
+++ packages/design-system/src/molecules/rightSidebar/index.tsx
@@ -1,5 +1,4 @@
-import { View } from '@gluestack-ui/react'
-import { Dimensions, Platform, StyleSheet } from 'react-native'
+import { Dimensions, Platform, StyleSheet, View } from 'react-native'
 import { PropsWithChildren } from 'react'
 
 import { useThemeV2 } from '../../hooks'
