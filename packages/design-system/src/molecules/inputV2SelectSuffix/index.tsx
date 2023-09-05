4d5eb55fbd02e7290a442dc13249c42743db9cdb
Teimur Gasanov <teimur@tryroll.com>
2023-09-05 14:51:04 +0600

    Fix atoms imports in select suffix

--- packages/design-system/src/molecules/inputV2SelectSuffix/index.tsx
+++ packages/design-system/src/molecules/inputV2SelectSuffix/index.tsx
@@ -2,9 +2,8 @@ import { Platform, View } from 'react-native'
 import { useCallback, useRef } from 'react'
 import { Pressable } from '@gluestack-ui/react'
 import { useThemeV2 } from '../../hooks'
-import { TypographyV2 } from '../../atoms/typographyV2'
 import { container, spacing } from '../../styles'
-import { Icon } from '../../atoms/icon'
+import { Icon, TypographyV2 } from '../../atoms'
 import { SelectV2, SelectV2Props } from '../selectV2'
 
 export type InputV2SelectSuffixProps = Omit<SelectV2Props, 'renderReference'>
